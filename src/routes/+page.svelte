8efd02583606bb0efbc46e9ef92a327b7b586832
Gregory Gaskill <gregory@chronon.com>
2023-10-12 15:10:43 -0400

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+  import images from "$lib/images.json";
+  let base = "https://imagedelivery.net/DvVl0mheSGO8iloS0s-G0g";
+  let variant = "default";
+</script>
+
+<h1 class="text-4xl font-bold underline">chrononagram</h1>
+
+<div>
+  {#each images as image}
+    <img loading="lazy" src="{base}/{image.id}/{variant}" alt={image.caption} />
+  {/each}
+</div>
