b2b7b07553c494de207a6adedef2e941aa0c2e1c
Gregory Gaskill <gregory@chronon.com>
2023-10-22 16:39:58 -0400

    Update layouts, add .env support for vars

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,25 @@
-<div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+<script>
+  import { PUBLIC_USER_NAME, PUBLIC_USER_AVATAR } from "$env/static/public";
+</script>
+
+<div class="mx-auto max-w-6xl sm:px-6 lg:px-8">
   <div class="border-b border-gray-200 bg-white px-4 py-5 sm:px-6 sticky top-0">
-    <div class="-ml-4 -mt-2 flex flex-wrap items-center justify-between sm:flex-nowrap">
+    <div
+      class="-ml-4 -mt-2 flex flex-wrap items-center justify-between sm:flex-nowrap"
+    >
       <div class="ml-4 mt-2">
-        <h3 class="text-base font-semibold leading-6 text-gray-900">
-          chrononagram
+        <h3 class="text-xl font-semibold text-sky-800 uppercase">
+          {PUBLIC_USER_NAME}
         </h3>
       </div>
-      <div class="ml-4 mt-2 flex-shrink-0">X</div>
+      <div class="ml-4 mt-2 flex-shrink-0">
+        <img
+          loading="lazy"
+          src={PUBLIC_USER_AVATAR}
+          alt={PUBLIC_USER_NAME}
+          class="inline-block rounded-full w-8 h-8 mr-2"
+        />
+      </div>
     </div>
   </div>
   <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,49 @@
 <script>
-  import InfiniteScroll from "$lib/InfiniteScroll.svelte";
   import images from "$lib/images.json";
-  const base = "https://imagedelivery.net/DvVl0mheSGO8iloS0s-G0g";
-  const variant = "default";
+  import InfiniteScroll from "$lib/InfiniteScroll.svelte";
+
+  import {
+    PUBLIC_IMG_BASE,
+    PUBLIC_IMG_VARIANT,
+    PUBLIC_USER_NAME,
+    PUBLIC_USER_AVATAR,
+  } from "$env/static/public";
 
   let increment = 2;
   let count = increment;
   const loadMore = () => (count += increment);
 </script>
 
-<div class="flex flex-col items-center">
-  {#each images.slice(0, count) as image}
-    <div class="flex justify-center w-full mt-8 md:p-8 rounded-lg bg-gray-50">
+{#each images.slice(0, count) as image}
+  <div class="mb-10 max-w-5xl rounded-lg mx-auto">
+    <div class="p-2 flex items-center">
       <img
         loading="lazy"
-        src="{base}/{image.id}/{variant}"
-        alt={image.caption}
+        src={PUBLIC_USER_AVATAR}
+        alt={PUBLIC_USER_NAME}
+        class="inline-block rounded-full w-8 h-8 mr-2"
       />
+      <p class="font-semibold">{PUBLIC_USER_NAME}</p>
     </div>
-  {/each}
-  <InfiniteScroll
-    hasMore={count < images.length}
-    threshold={200}
-    on:loadMore={loadMore}
-  />
-</div>
+    <img
+      loading="lazy"
+      src="{PUBLIC_IMG_BASE}/{image.id}/{PUBLIC_IMG_VARIANT}"
+      alt={image.caption}
+      class="mx-auto"
+    />
+    <div class="p-2">
+      {#if image.caption}
+        <p class="leading-tight mb-2">{image.caption}</p>
+      {/if}
+      <p class="text-sm text-gray-400">
+        Posted {new Date(image.uploaded).toLocaleString()}
+      </p>
+    </div>
+  </div>
+{/each}
+
+<InfiniteScroll
+  hasMore={count < images.length}
+  threshold={200}
+  on:loadMore={loadMore}
+/>
