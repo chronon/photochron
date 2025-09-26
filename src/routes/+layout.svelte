757d41a2a7694dad0138cf736c1b73e53114b954
Gregory Gaskill <gregory@chronon.com>
2025-09-26 09:07:24 -0400

    Fix top bar layering

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -21,7 +21,7 @@
 
 <div class="app" class:loaded>
 	<div class="mx-auto max-w-6xl sm:px-6 lg:px-8">
-		<div class="sticky top-0 border-b border-gray-200 bg-white px-4 py-5 sm:px-6">
+		<div class="sticky top-0 z-10 border-b border-gray-200 bg-white px-4 py-5 sm:px-6">
 			<div class="-mt-2 -ml-4 flex flex-wrap items-center justify-between sm:flex-nowrap">
 				<div class="mt-2 ml-4">
 					<h3 class="text-xl font-semibold text-sky-800 uppercase">
